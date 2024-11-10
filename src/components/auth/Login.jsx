67da2e71f6e515fa7c203ba10785534784b76255
rafaelsava <rafaelsava@unisabana.edu.co>
2024-11-10 16:51:01 -0500

    Refactor user context usage and enhance login flow; improve token verification in AddVehicle component

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthLayout from "../reusable/AuthLayout"; 
+import { useUser } from "./UserContext";
+import AuthLayout from "../reusable/AuthLayout";
 import CustomInput from "../reusable/CustomInput";
 import CustomButton from "../reusable/CustomButton";
 import LinkText from "../reusable/LinkText";
-import Swal from 'sweetalert2'; // Importamos SweetAlert2
+import Swal from 'sweetalert2';
+import { Eye, EyeOff } from 'lucide-react'; // Importa los iconos de Lucide
 
 const Login = () => {
+  const { user, setUser } = useUser();
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar contraseña
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  // Función para verificar la validez del token
   const verifyToken = async (token) => {
     try {
       const response = await fetch(
@@ -20,14 +23,13 @@ const Login = () => {
         {
           method: "GET",
           headers: {
-            Authorization: `Bearer ${token}`, // Enviar el token en los headers
+            Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
           },
         }
       );
 
       if (response.ok) {
-        // Si el token es válido, redirigir al usuario a la página de lista de viajes
         navigate("/trip-list");
       }
     } catch (error) {
@@ -45,16 +47,12 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Verificar que ambos campos estén completos
     if (!mail || !password) {
       setError("Por favor completa ambos campos");
       return;
     }
 
-    const loginData = {
-      mail,
-      password,
-    };
+    const loginData = { mail, password };
 
     try {
       const response = await fetch(
@@ -80,20 +78,19 @@ const Login = () => {
       }
 
       const data = await response.json();
+      setUser({ id: data.userId });
       console.log("Respuesta del backend:", data);
 
-      // Almacenar el token JWT en localStorage
       localStorage.setItem("token", data.token);
 
-      // Mostrar alerta de éxito y redirigir
       Swal.fire({
         title: "Inicio de sesión exitoso",
         text: "Redirigiendo a la lista de viajes...",
         icon: "success",
         showConfirmButton: false,
-        timer: 2000, // Esperar 2 segundos antes de redirigir
+        timer: 2000,
       }).then(() => {
-        navigate("/trip-list"); // Redirigir después de la alerta
+        navigate("/trip-list");
       });
     } catch (error) {
       console.error("Error en la solicitud:", error);
@@ -115,13 +112,23 @@ const Login = () => {
           value={mail}
           onChange={(e) => setMail(e.target.value)}
         />
-        <CustomInput
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error && <p className="text-red-500 text-sm">{error}</p>} 
+        <div className="relative">
+          <CustomInput
+            type={showPassword ? "text" : "password"} // Cambia el tipo según el estado
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+          >
+            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+          </button>
+        </div>
+        {error && <p className="text-red-500 text-sm">{error}</p>}
         <CustomButton
           type="submit"
           className="bg-orange-500 text-white py-2 px-6 rounded-lg hover:bg-orange-600"
