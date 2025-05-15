e67ab394563911b7fd92be9ca1915fb723569c5f
FerminEscalona <ferminesgu@unisabana.edu.co>
2025-05-15 10:27:45 -0500

    CI/CD pipeline

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -3,11 +3,30 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // URL donde arranca tu app en CI
     baseUrl: 'http://localhost:5173',
+
+    // Busca todos los *.cy.js y *.cy.jsx en cypress/e2e
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx}',
+
+    // Soporte y fixtures
     supportFile: 'cypress/support/e2e.js',
+    fixturesFolder: 'cypress/fixtures',
+
+    // Dónde dejar capturas y vídeos
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+
+    // Reporter JUnit para GitLab
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'cypress/results/junit-[hash].xml'
+    },
+
     setupNodeEvents(on, config) {
-      // aquí tus listeners si los necesitas
+      // aquí puedes enganchar listeners de cobertura,
+      // plugin de imagenes, etc.
+      return config
     },
   },
 })
